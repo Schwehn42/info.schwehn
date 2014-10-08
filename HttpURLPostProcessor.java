3ee2b1c44900b4bddcaa49fb5153dbff11cbd9a0
Schwehn42 <jakob.epic@web.de>
2014-10-08 16:17:32 +0200

    Update HttpURLPostProcessor.java

--- HttpURLPostProcessor.java
+++ HttpURLPostProcessor.java
@@ -1,46 +1,43 @@
-// HI I'M GET
+// HI I'M POST
 
 package kohlhans;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.io.OutputStreamWriter;
 import java.net.URL;
+import java.net.URLConnection;
 
-public class HttpURLGetProcessor extends Connection
+public class HttpURLPostProcessor extends Connection
 {
-
-	private final String USER_AGENT = "Mozilla/5.0";
-
-	// HTTP GET request
-	public void sendGet (String url) throws Exception
+	// HTTP POST request
+	public void sendPost (String url, String[] query) throws Exception
 	{
+		QueryParser parser = new QueryParser();
+		String urlParameters = parser.decode(query);
 		URL URLobj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) URLobj.openConnection();
+		URLConnection conn = URLobj.openConnection();
 
-		// optional default is GET
-		con.setRequestMethod("GET");
+		conn.setDoOutput(true);
 
-		// add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
+		OutputStreamWriter writer = new OutputStreamWriter(
+		        conn.getOutputStream());
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to: " + url);
-		System.out.println("Response Code : " + responseCode);
+		System.out.println("Sending 'POST' request to: " + url);
+		System.out.println("Parameters: " + urlParameters);
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-		        con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
+		writer.write(urlParameters);
+		writer.flush();
 
-		while ((inputLine = in.readLine()) != null)
+		String line;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+		        conn.getInputStream()));
+
+		while ((line = reader.readLine()) != null)
 		{
-			response.append(inputLine);
+			System.out.println(line);
 		}
-		in.close();
-
-		// print result
-		System.out.println(response.toString());
-
+		writer.close();
+		reader.close();
 	}
 }
